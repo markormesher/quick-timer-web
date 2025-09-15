68daa04de3d1c3844455ddc924b85318d574c1f0
Mark Ormesher <me@markormesher.co.uk>
2025-09-15 15:07:03 -0700

    feat: preferences and (untested) vibrate (#5)
    * setters for pref-backed state
    
    * read vibrate pref and (maybe) vibrate on completion
    
    * rename

---
+++ src/components/prefs.tsx
@@ -0,0 +1,70 @@
+import React, { ReactElement } from "react";
+import { Icon } from "./icon.js";
+
+function PrefBar(): ReactElement {
+  const [theme, toggleTheme] = usePrefToggle("theme", ["auto", "light", "dark"]);
+  const [soundEnabled, toggleSoundEnabled] = usePrefToggle("sound-enabled", ["on", "off"]);
+  const [vibrateEnabled, toggleVibrateEnabled] = usePrefToggle("vibrate-enabled", ["on", "off"]);
+
+  const themeIcons: Record<string, string> = {
+    auto: "brightness_auto",
+    light: "light_mode",
+    dark: "dark_mode",
+  };
+
+  const soundEnabledIcons: Record<string, string> = {
+    on: "volume_up",
+    off: "volume_off",
+  };
+
+  const vibrateEnabledIcons: Record<string, string> = {
+    on: "mobile_vibrate",
+    off: "mobile",
+  };
+
+  return (
+    <div className={"pref-bar"}>
+      <span onClick={toggleTheme}>
+        <Icon name={themeIcons[theme] ?? ""} />
+      </span>
+      <span onClick={toggleSoundEnabled}>
+        <Icon name={soundEnabledIcons[soundEnabled] ?? ""} />
+      </span>
+      <span onClick={toggleVibrateEnabled}>
+        <Icon name={vibrateEnabledIcons[vibrateEnabled] ?? ""} />
+      </span>
+    </div>
+  );
+}
+
+function usePrefToggle(key: string, values: string[]): [string, () => void] {
+  const [reactState, setReactState] = React.useState("");
+
+  React.useEffect(() => {
+    const initial = localStorage.getItem(key);
+    if (initial && values.includes(initial)) {
+      setReactState(initial);
+    } else {
+      setReactState(values[0] ?? "");
+    }
+  }, [key, values, setReactState]);
+
+  const toggle = () => {
+    const currIdx = values.indexOf(reactState);
+    let newIdx = 0;
+    if (currIdx >= 0 && currIdx <= values.length - 2) {
+      newIdx = currIdx + 1;
+    }
+
+    setReactState(values[newIdx] ?? "");
+    localStorage.setItem(key, values[newIdx] ?? "");
+  };
+
+  return [reactState, toggle];
+}
+
+function readPref(key: string): string {
+  return localStorage.getItem(key) ?? "";
+}
+
+export { PrefBar, readPref };
